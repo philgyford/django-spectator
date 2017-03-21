3a62e761e3f2fe4941ff5fc7ad15badbdb3abab3
Phil Gyford <phil@gyford.com>
2017-03-21 14:56:23 +0000

    Add methods to get a Creators' movies, plays and playproductions.
    And use them in the CreatorDetail template.

--- spectator/models/core.py
+++ spectator/models/core.py
@@ -60,26 +60,30 @@ class Creator(TimeStampedModelMixin, models.Model):
         creator = Creator.objects.get(pk=1)
 
         # Just Publication titles:
-        for publication in creator.publications.all():
+        for publication in creator.publications.distinct():
             print(publication.title)
+
+        # You can do similar to that to get lists of `concerts`, `movies`,
+        # `plays` and `play_productions` the Creator was involved with.
+
 
         # Or Publications including the Creator and their role:
         for role in creator.publication_roles.all():
             print(role.publication, role.creator, role.role_name)
 
-        # Similarly for Concerts:
+        # Similarly for Concert roles:
         for role in creator.concert_roles.all():
             print(role.concert, role.creator, role.role_name)
 
-        # And for Movies:
+        # And for Movie roles:
         for role in creator.movie_roles.all():
             print(role.movie, role.creator, role.role_name)
 
-        # And for Plays:
+        # And for Play roles:
         for role in creator.play_roles.all():
             print(role.play, role.creator, role.role_name)
 
-        # And for PlaysProductions:
+        # And for PlaysProduction roles:
         for role in creator.play_production_roles.all():
             print(role.production, role.production.play, role.creator, role.role_name)
     """
@@ -115,4 +119,107 @@ class Creator(TimeStampedModelMixin, models.Model):
             return 'person'
         else:
             return 'thing'
+
+    def get_movies(self):
+        """
+        A list of all the Movies the Creator worked on.
+        Each one also has these properties:
+        * `creator_roles` - QuerySet of MovieRole objects for this Creator.
+        * `creator_role_names` - List of the role_names (if any this Creator
+            had. Note, this could be empty if none of the roles have names.
+        """
+        movies = []
+        for movie in self.movies.distinct():
+            movie.creator_roles = movie.roles.filter(creator=self)
+            movie.creator_role_names = []
+            for role in movie.creator_roles:
+                if role.role_name:
+                    movie.creator_role_names.append(role.role_name)
+            movies.append(movie)
+        return movies
+
+    def get_plays(self):
+        """
+        A list of all the Plays the Creator worked on.
+        Each one also has these properties:
+        * `creator_roles` - QuerySet of PlayRole objects for this Creator.
+        * `creator_role_names` - List of the role_names (if any this Creator
+            had. Note, this could be empty if none of the roles have names.
+        """
+        plays = []
+        for play in self.plays.distinct():
+            play.creator_roles = play.roles.filter(creator=self)
+            play.creator_role_names = []
+            for role in play.creator_roles:
+                if role.role_name:
+                    play.creator_role_names.append(role.role_name)
+            plays.append(play)
+        return plays
+
+    def get_play_productions(self):
+        """
+        A list of all the PlayProductions the Creator worked on.
+        Each one also has these properties:
+        * `creator_roles` - QuerySet of PlayProductionRole objects for this
+            Creator.
+        * `creator_role_names` - List of the role_names (if any this Creator
+            had. Note, this could be empty if none of the roles have names.
+        """
+        productions = []
+        for production in self.play_productions.distinct():
+            production.creator_roles = production.roles.filter(creator=self)
+            production.creator_role_names = []
+            for role in production.creator_roles:
+                if role.role_name:
+                    production.creator_role_names.append(role.role_name)
+            productions.append(production)
+        return productions
+
+    def get_plays_and_productions(self):
+        """
+        Returns a list that combines Plays and PlayProductions, ordered by
+        the Plays' `title_sort` field.
+
+        It includes all Plays that the Creator had a role in, and all Plays
+        which has PlayProductions in which the Creator had a role.
+
+        Something like:
+
+            [
+                {
+                    'play': Play1,
+                    'productions': [ Production1, Production2, ],
+                },
+                {
+                    'play': Play2,
+                    'productions': [],
+                },
+            ]
+
+        Here, the Creator was in two PlayProductions of Play1 (and may have
+        had a role in creating Play1 itself). And also maybe wrote Play2 but
+        didn't have a role in any PlayProductions of it.
+
+        All Plays and PlayProductions have `creator_roles` and
+        `creator_role_names` properties, as in `self.get_plays()` and
+        `self.get_play_productions`.
+        """
+        plays = self.get_plays()
+        # Make an initial dict, keyed by IDs of the plays:
+        both = {p.id:{'play':p,'productions':[],} for p in plays}
+
+        for production in self.get_play_productions():
+            play_id = production.play_id
+            if production.play_id in both:
+                both[play_id]['productions'].append(production)
+            else:
+                both[play_id] = {
+                    'play': production.play,
+                    'productions': [ production, ]
+                }
+
+        both = list(both.values())
+        both.sort(key=lambda x: x['play'].title_sort)
+
+        return both
 
