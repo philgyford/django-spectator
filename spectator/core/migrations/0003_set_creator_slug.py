3b5c26db02a3b895af8c6da9ef9e5b79274a6857
Phil Gyford <phil@gyford.com>
2018-01-03 11:43:32 +0000

    Fix error in Creator migration
    Due to manually tweaking old migrations.
    Tests in django-hines were complaining about the migration trying
    to add Creator.slug a second time.

--- spectator/core/migrations/0003_set_creator_slug.py
+++ spectator/core/migrations/0003_set_creator_slug.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
+        migrations.AlterField(
             model_name='creator',
             name='slug',
             field=models.SlugField(blank=True, max_length=10),
