692c789b7b50cdcf938563f0ec4c6f4a7369ade1
Phil Gyford <phil@gyford.com>
2018-01-17 16:39:56 +0000

    Bump to 5.1.1

--- spectator/__init__.py
+++ spectator/__init__.py
@@ -1,9 +1,8 @@
 __title__ = 'Django Spectator'
-__version__ = '5.1.0'
+__version__ = '5.1.1'
 __author__ = 'Phil Gyford'
 __author_email__ = 'phil@gyford.com'
 __license__ = 'MIT'
 
 VERSION = __version__  # synonym
 TITLE = __title__  # synonym
-
