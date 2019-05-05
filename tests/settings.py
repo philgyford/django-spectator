05fa4ea1f0802789aaa7fac7e3df863be1b5efbd
Phil Gyford <phil@gyford.com>
2019-05-05 16:52:57 +0100

    Update tests for python/django versions
    Stopped testing in python 3.4 and Django 2.0.
    Started testing in python 3.7 and Django 2.2.

--- tests/settings.py
+++ tests/settings.py
@@ -22,6 +22,16 @@ INSTALLED_APPS = (
     'tests.core.fields',
 )
 
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
 ROOT_URLCONF = 'tests.urls'
 
 TEMPLATES = [
